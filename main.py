3ddb1ab5aeaa1230525f5a8694a420e7fdf37cbc
vermosen <vermosen.jm@free.fr>
2015-02-12 13:52:01 -0500

    initial commit

---
+++ main.py
@@ -0,0 +1,11 @@
+import urllib3
+
+# step 1 - connect to the bls website
+http = urllib3.PoolManager()
+req = http.request('GET', 'http://download.bls.gov/pub/time.series/')
+
+# step 2 - create a file
+f = open('test.txt', 'wb')
+f.write(req.data)
+f.close()
+req.release_conn()
